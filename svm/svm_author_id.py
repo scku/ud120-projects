5ef0befb5a382a75a54fdacde496fb2e009dfa0d
Stanley Ku <mr.stanleyku@gmail.com>
2015-05-04 18:35:58 -0700

    finish lession 2

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -14,7 +14,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,6 +27,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+clf = svm.SVC(kernel='rbf', C=10000)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s" # 3.3s
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s" # 3.3s
+
+acc = accuracy_score(pred, labels_test)
+print "Accuracy:{0}".format(acc) # 
+
+print pred[10], pred[26], pred[50]
+print sum(pred)
 
 #########################################################
 
