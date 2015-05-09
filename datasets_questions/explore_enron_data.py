cba06d92fcd3a6ddcd29b8fc88a5b7131449f0d8
Stanley Ku <mr.stanleyku@gmail.com>
2015-05-08 18:20:02 -0700

    finish lesson 5

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,18 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print "Data points:{0}".format(len(enron_data)) # 146
 
+n_poi = 0
+for i in enron_data:
+	if enron_data[i]["poi"]:
+		n_poi += 1
+print "Number of poi:{0}".format(n_poi) # 18
+
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"] # 11
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+print enron_data["SKILLING JEFFREY K"]["total_payments"]
+print enron_data["LAY KENNETH L"]["total_payments"] # 103559793
+print enron_data["FASTOW ANDREW S"]["total_payments"]
